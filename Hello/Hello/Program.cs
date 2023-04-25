f5c5723cb907c40e810c88542d8eb2b59df4af85
rk37946 <126868265+rk37946@users.noreply.github.com>
2023-04-25 09:20:26 +0530

    Practice for C#

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,68 @@
+﻿                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Below line prints it in the console
+            //  Console.WriteLine("Hello C# lovers!!!");
+
+
+            //Comments in C#
+            // This is single line comment
+            /* This is used foir multui line comment. */
+
+
+            // Data types in c#
+            // int x = 34; //int data type
+            // long i=345454643; //long data type
+            // float y = 4.5F; //floating point 
+            // double z= 4.52; //double type
+            // char a = 'X'; // character type
+            // bool isValid = true; //boolean type
+            // string str = "I used to codse in Rust";  //string type
+            // Console.WriteLine("My name is rinki and the number is " + x);
+            // Console.WriteLine("Integer is " + x + " Float is " + y +  " Char is " + a + " Boolean is " + isValid + " String is " + str);
+
+
+            // calculating size of any variable
+            // Console.WriteLine("The size of integer is " + sizeof(int) + "bytes.");
+
+
+            // Taking string input from user
+            // Console.WriteLine("Enter a string :");
+            // string str1 = Console.ReadLine(); // takes input from user and assign it to str1
+            // Console.WriteLine("You have entered : "+ str1); // Printing the user input to the console          
+
+
+
+            // type casting - implicit and explicit
+            // 1. Implicit type casting
+            // char to int to long to float to double
+            // int x = 5;
+            // double y = x; // implicit type casting
+            // Console.WriteLine(y);
+            // 2. Explicit Type casting
+            // int a = (int)3.5; // double is converted into integer through explicit type casting
+            // Console.WriteLine(a); // will print only 3
+
+
+
+            //
+
+
+
+
+
+            
+            Console.Read();
+
+        }
+    }
+}
